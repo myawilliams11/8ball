d8082b70a1e1786322664bf73b880caf73f26b81
Mya <35534673+myawilliams11@users.noreply.github.com>
2018-02-21 15:49:38 -0500

    Add files via upload

--- slot.js
+++ slot.js
@@ -1,31 +1,12 @@
-$(document).ready(function(){
+// $(document).ready(function(){
   
-    var magic8Ball = {};
-    magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
-   
-    magic8Ball.getAnswer = function(question)
-    {
-      var randomNumber = Math.random();
-      var randomAnswer = Math.floor(randomNumber * this.listofanswers.length);
-      var answer = this.listofanswers[randomAnswer];
-      
-      $("#8ball").effect( "shake" );
-      $("#answer").text( answer );
-      $("#answer").fadeIn(3000);
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
-  
-      console.log(question);
-      console.log(answer);
-    };
-    $("#answer").hide();
-  
-    var onClick = function()
-    {
-      $("#answer").hide();
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
-      var question = prompt("What do you want to know?");
-      magic8Ball.getAnswer(question);
-    };
     
-    $("#questionButton").click( onClick );
-  });
+    var genieCommands = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+   
+
+document.getElementById("button").onclick= function(){
+
+    var randomAnswer = genieCommands[Math.floor(Math.random() * genieCommands.length)];
+    var fortune = document.getElementById("fortune");
+    fortune.textContent = randomAnswer;
+}
